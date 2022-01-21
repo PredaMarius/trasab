1f8bdcdaf4b2d78ec3664108c8db8e1852fa0a4f
PredaMarius <predamarius2007@yahoo.com>
2022-01-21 11:15:36 +0200

    21022022 1115

--- front/src/config/strapi.js
+++ front/src/config/strapi.js
@@ -1,2 +1,5 @@
-export const strapiUrl='https://softdivisionasus.local';
-export const strapiGraphqlUrl='https://softdivisionasus.local/graphql';
+export const strapiUrl='http://192.168.2.143:1337';
+export const strapiGraphqlUrl='http://192.168.2.143:1337/graphql';
+
+// export const strapiUrl='https://softdivisionasus.local';
+// export const strapiGraphqlUrl='https://softdivisionasus.local/graphql';
